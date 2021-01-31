a6e1d1fd9dfbf05a4ea84eee6d84aec130a62a61
陈旭斌 <xbchen@travelsky.com>
2021-01-31 11:49:53 +0800

    创建项目

---
+++ components/field/index.js
@@ -0,0 +1,35 @@
+// components/filed/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    list: [{
+        name: 'xx',
+        data: '0000'
+      },
+      {
+        name: 'xxx',
+        data: '00001'
+      },
+      {
+        name: 'xxxx',
+        data: '00002'
+      }
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
